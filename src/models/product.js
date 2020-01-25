2572e189b9b8879879ee54d967b8233aad2a4484
Felipe Barros <felipetech@tuta.io>
2020-01-25 10:33:29 -0300

    Atualizando model de produto

--- src/models/product.js
+++ src/models/product.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema({
-  name: String,
-  price: Number
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  }
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
